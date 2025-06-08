6832b14563d1f19c56d1756250c43069113adbd9
jd115410 <erin.massingham@my.jcu.edu.au>
2025-06-08 23:12:22 +1000

    Reading and writing into and from text files

---
+++ prac_03/files.py
@@ -0,0 +1,20 @@
+out_file = open("name.txt", "w")
+name = input("Enter your name: ")
+print(name, file=out_file)
+out_file.close()
+
+in_file = open("name.txt", "r")
+name_in_file = in_file.read().strip()
+in_file.close()
+print(f"Hi {name_in_file}!")
+
+with open("numbers.txt", "r") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+print(first_number + second_number)
+
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        total += int(line)
+print(total)
