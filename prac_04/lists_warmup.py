52c3c18751acf29346159103f69c35e5ea3de9b2
jd115410 <erin.massingham@my.jcu.edu.au>
2025-06-11 11:41:03 +1000

    Commit work with lists

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+#numbers[0]: index position 0 in list 'numbers', has value 3
+#numbers[-1]: index position from last positions forward, has value 2
+#numbers[3]: index position 3 (0, 1, 2, 3), value 1
+#numbers[:-1]: all index position up to but not including position -1, value [3, 1, 4, 1, 5, 9]
+#numbers[3:4]: all index positions including starting index (3) but excluding ending index (4), value 1
+#5 in numbers: will check list numbers for 5 returning True if 5 is there or False if not, returns True
+#7 in numbers: will check list numbers for 5 returning True if 7 is there or False if not, returns False
+#"3" in numbers: will check list numbers for the string 3 (numbers = ("3", ...)), returns False
+#numbers + [6, 5, 3]: updates numbers to include the new values, returns [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
+numbers[0] = "Ten"
+numbers[-1] = 1
+print(numbers[3:])
+print(f"9 is an element of numbers?: {9 in numbers}")
