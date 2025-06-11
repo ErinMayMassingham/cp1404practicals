ce2a350dc6c39bd40563602406d33d7141819a33
jd115410 <erin.massingham@my.jcu.edu.au>
2025-06-11 13:33:30 +1000

    commmit refactoring and subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,28 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    print_details(data)
+
+
+def print_details(data):
+    for details in data:
+        print(f"{details[0]} is taught by {details[1]} and has {details[2]} students")
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    details = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        #print(line)  # See what a line looks like
+        #print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        #print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        details.append(parts)
     input_file.close()
+    return details
 
 
 main()
