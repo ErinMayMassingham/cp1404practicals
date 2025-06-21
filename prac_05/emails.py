a99a864facaa476572b04dc1052e5888bf0f2e38
jd115410 <erin.massingham@my.jcu.edu.au>
2025-06-22 00:28:43 +1000

    Complete prac 5 word emails

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,16 +1,14 @@
-"""
-CP1404/CP5632 Practical - Suggested Solution
-Email to name dictionary
-"""
-
+""" word occurrences
+Estimate : 30 minutes
+Actual: 45 minutes """
 
 def main():
-    """Create dictionary of emails-to-names."""
+    """Create dictionary of emails changing to names."""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
         name = get_name_from_email(email)
-        confirmation = input(f"Is your name {name}? (Y/n) ")
+        confirmation = input(f"Is your name {name}? (Y/N) ")
         if confirmation.upper() != "Y" and confirmation != "":
             name = input("Name: ")
         email_to_name[email] = name
@@ -21,11 +19,10 @@ def main():
 
 
 def get_name_from_email(email):
-    """Extract expected name from email address."""
+    """Extract name from email"""
     prefix = email.split('@')[0]
     parts = prefix.split('.')
     name = " ".join(parts).title()
     return name
-
 
 main()
