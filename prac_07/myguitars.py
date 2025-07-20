aff1ce6c535d700d3941fc893b0379b72a6b966e
jd115410 <erin.massingham@my.jcu.edu.au>
2025-07-20 22:11:42 +1000

    Commit load and display guitars

---
+++ prac_07/myguitars.py
@@ -0,0 +1,27 @@
+from guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+def main():
+    """Display guitars loaded from file"""
+    print("These are the guitars loaded:")
+    guitars = load_guitars(FILENAME)
+    display_guitars(guitars)
+
+def load_guitars(filename):
+    """Load guitars from CSV into a list of Guitars"""
+    guitars = []
+    with open(filename, "r") as file:
+        for line in file:
+            parts = line.strip().split(",")
+            name, year, cost = parts[0], int(parts[1]), float(parts[2])
+            guitars.append(Guitar(name, year, cost))
+    return guitars
+
+def display_guitars(guitars):
+    """Print all guitars in list"""
+    for i, guitar in enumerate(guitars, start=1):
+        print(f"Guitar {i}: {guitar}")
+
+
+main()
