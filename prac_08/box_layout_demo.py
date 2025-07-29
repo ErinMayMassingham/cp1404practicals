c4666184922924100d7822f036369625f0c6f016
jd115410 <erin.massingham@my.jcu.edu.au>
2025-07-29 21:33:45 +1000

    Commit greeting and clear features in BoxLayout app

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,11 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        name = self.root.ids.input_name.text.strip()
+        self.root.ids.output_label.text = f"Hello {name}" if name else "Hello"
 
-BoxLayoutDemo().run()
+    def clear_fields(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
